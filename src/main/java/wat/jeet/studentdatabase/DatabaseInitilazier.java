0fb08ea56d698d849d583a7da7bf7c92566acc8c
Student <Student@10.7.10.70>
2025-12-16 13:00:43 +0100

    fixed

--- src/main/java/wat/jeet/studentdatabase/DatabaseInitilazier.java
+++ src/main/java/wat/jeet/studentdatabase/DatabaseInitilazier.java
@@ -12,20 +12,26 @@ import java.sql.Statement;
  *
  * @author Student
  */
+
 public class DatabaseInitilazier {
 
     private static final String DB_URL = "jdbc:h2:~/testdb";
 
     public static void initialize() {
-        try (Connection connection = DriverManager.getConnection(DB_URL, "sa", ""); Statement statement = connection.createStatement()) {
-            String createTableSQL = """
+        try {
+            Class.forName("org.h2.Driver");
+
+            try (Connection connection = DriverManager.getConnection(DB_URL, "sa", ""); Statement statement = connection.createStatement()) {
+
+                String createTableSQL = """
 CREATE TABLE IF NOT EXISTS users (
 id BIGINT AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(255) NOT NULL,
 email VARCHAR(255) NOT NULL
 )
 """;
-            statement.execute(createTableSQL);
+                statement.execute(createTableSQL);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
